e44b73d50babc9bc91d52e880cc9a40239067117
guohong630 <guohong_bs@hotmail.com>
2015-10-14 11:28:16 -0400

    Integrated Google map

---
+++ iphone/GoogleMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  GoogleMapViewController.h
+//  Compass[transparent]
+//
+//  Created by Hong Guo on 10/13/15.
+//  Copyright © 2015 dmiau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GoogleMapViewController : UIViewController
+
+@end
